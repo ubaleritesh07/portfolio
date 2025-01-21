ee4391a1b33cb7b7016c6254b6cc2ccd7332f020
Ritesh Ubale <ubaleritesh.4@gmail.com>
2025-01-21 20:30:49 +0530

    new file added

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "ubaleritesh57@gmail.com"; 
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    $fullMessage = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        echo "Thank you, $name! Your message has been sent.";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+}
+?>
